b2a1cb1e7da43b76793af88800f30e0a17b97365
KingTeslaw <ehsan.nasiri1384@gmail.com>
2025-06-25 03:23:20 +0330

    add new ui for sidebar

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,3 +4,13 @@ let ehsan = 2;
 let w = 2;
 
 let ehsanjun = 2;
+
+
+let mobileSidebar = document.querySelector(".mobile-sidebar");
+let openSidebar = document.querySelector(".open-sidebar");
+
+mobileSidebar.addEventListener("click",(e)=>{
+    // open menu
+
+
+});
