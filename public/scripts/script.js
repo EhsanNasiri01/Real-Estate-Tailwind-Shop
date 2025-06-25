f42cbab257f12c2368544f6407ea0bccb7be807f
KingTeslaw <ehsan.nasiri1384@gmail.com>
2025-06-25 03:49:33 +0330

    Add new Sidebar menu ui

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,11 +6,21 @@ let w = 2;
 let ehsanjun = 2;
 
 
-let mobileSidebar = document.querySelector(".mobile-sidebar");
-let openSidebar = document.querySelector(".open-sidebar");
-
-mobileSidebar.addEventListener("click",(e)=>{
+let mobileMenuBtn = document.querySelector(".mobileMenuBtn");
+let sidebarMenu = document.querySelector(".sidebarMenu");
+let closeSidebar = document.querySelector(".closeSidebar");
+mobileMenuBtn.addEventListener("click",(e)=>{
     // open menu
-
+    console.log("ok2");
+    sidebarMenu.className = "open-sidebarmenu";
+});
+closeSidebar.addEventListener("click",(e)=>{
+    console.log("Ok");
+    console.log(sidebarMenu);
+    // close menu
+    sidebarMenu.classList.add("invisible");
+    sidebarMenu.classList.add("opacity-0");
+    sidebarMenu.classList.add("hidden");
 
 });
+
